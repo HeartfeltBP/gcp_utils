4e7bd0cd98d3a394c72ce70fb2ed314d4ac0aa44
camhpj <cjohnson23@gwu.edu>
2023-02-13 14:35:13 -0500

    updated onValidSample deployment to be on update; removed old test data

--- scripts/deploy_onValidSample.sh
+++ scripts/deploy_onValidSample.sh
@@ -1,6 +1,6 @@
 gcloud functions deploy onValidSample \
   --entry-point onValidSample \
   --runtime python39 \
-  --trigger-event "providers/cloud.firestore/eventTypes/document.create" \
+  --trigger-event "providers/cloud.firestore/eventTypes/document.update" \
   --trigger-resource "projects/heartfelt-0/databases/(default)/documents/processed_samples/{sample}" \
   --memory=512MB
