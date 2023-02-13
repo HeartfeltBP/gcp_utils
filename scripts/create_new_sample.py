d215f3498fb3ad9910f1590b59272de33b2d01e8
camhpj <cjohnson23@gwu.edu>
2023-02-13 14:32:05 -0500

    removed unused script

--- scripts/create_new_sample.py
+++
@@ -1,9 +0,0 @@
-from gcp_utils.data import raw_valid_sample
-from firebase_admin import credentials, firestore, initialize_app
-
-initialize_app()
-database = firestore.client()
-col = database.collection('heartfelt_data')
-
-sample = raw_valid_sample()
-col.add(sample)
