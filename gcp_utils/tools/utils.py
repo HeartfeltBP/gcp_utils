d5ee924bbac1b984e6105a7d356a71d9307f6a46
camhpj <cjohnson23@gwu.edu>
2023-02-12 19:27:48 -0500

    added type error to json casting function

--- gcp_utils/tools/utils.py
+++ gcp_utils/tools/utils.py
@@ -2,12 +2,14 @@ def default_to_json(x, name):
     if isinstance(x, int):
         data = {name: {'integerValue': x}}
         return data
-    if isinstance(x, str):
+    elif isinstance(x, str):
         data = {name: {'stringValue': x}}
         return data
-    if isinstance(x, bool):
+    elif isinstance(x, bool):
         data = {name: {'booleanValue': x}}
         return data
-    if isinstance(x, list):
+    elif isinstance(x, list):
         data = {'arrayValue': {'values': [{'doubleValue': xx} for xx in x]}}
         return data
+    else:
+        raise TypeError(f'Type \'{type(x)}\' is not a supported type')
