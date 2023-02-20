9562de774f18b00c6e4a731135c3437b918482be
camhpj <cjohnson23@gwu.edu>
2023-02-19 19:20:25 -0500

    fixed typo in deploy script

--- scripts/deploy_onNewFrame.sh
+++ scripts/deploy_onNewFrame.sh
@@ -3,4 +3,4 @@ gcloud functions deploy onNewFrame \
   --runtime python39 \
   --trigger-event "providers/cloud.firestore/eventTypes/document.create" \
   --trigger-resource "projects/heartfelt-0/databases/(default)/documents/frames/{sample}" \
-  --memory=512MBcd
+  --memory=512MB
